12bf69ceb801f2a52cc9dab479611bc4231af6a1
kaidecoder <131601358+kaidecoder@users.noreply.github.com>
2023-12-22 08:46:32 -0500

    working on the win functionality

--- gameBoard.js
+++ gameBoard.js
@@ -1,50 +1,64 @@
 let gameBoard = document.querySelector(".game-board");
-let box = document.querySelectorAll(".box")
-let span = document.querySelectorAll("span")
-let info = document.querySelector(".game-info")
+let box = document.querySelectorAll(".box");
+let span = document.querySelectorAll("span");
+let info = document.querySelector(".game-info");
 
-let playerOne = "X"
-let playerTwo = "O"
+let playerOne = "X";
+let playerTwo = "O";
 
-function clearGameBoard(){
-    console.log("inside board clear function")
-    // gameBoard.classList.add("show") this gives flex-direction: column
-    span.forEach(item => {
-        item.innerHTML = ""
-    })
+function clearGameBoard() {
+  console.log("inside board clear function");
+  // gameBoard.classList.add("show") this gives flex-direction: column
+  span.forEach((item) => {
+    console.log(item.innerHTML);
+    item.style.opacity = "0";
+  });
 }
-clearGameBoard()
+clearGameBoard();
 
-function setInfo(){
-    info.innerHTML += "<br/><br />Player X goes first"
+function setInfo() {
+  info.innerHTML += "<br/><br />Player X goes first";
 }
 
-function startGame(){
-    console.log("inside start")
-    clearGameBoard()
-    setInfo()
+function startGame() {
+  console.log("inside start");
+  clearGameBoard();
+  setInfo();
 }
-startGame()
+startGame();
 
 /**
  * ! Click on any square to draw the X.  Check if there is something there already if so, message, otherwise, draw
-**/
-function playerXMoves(){
-    box.forEach(square => {
-        console.log("inside the box")
-        square.addEventListener("click", e => {
-            e.preventDefault()
-            if(square.innerHTML === ("X") || square.innerHTML === ("O")){
-                return
-            }else{
-                square.innerHTML = "X"
-            }
-        })
-    })
+ **/
+function playerXMoves() {
+  box.forEach((square) => {
+    console.log("inside the box");
+    square.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (square.innerHTML === "X" || square.innerHTML === "O") {
+        return;
+      } else {
+        square.innerHTML = "X";
+        checkforAWin("X");
+      }
+    });
+  });
 }
-playerXMoves()
 
-gameBoard.addEventListener("click", e => {
-    e.preventDefault()
-    // startGame()
-})
+//I need to say first item equal X
+function checkforAWin(player) {
+  span.forEach((item) => {
+    console.log(item.innerHTML);
+    for (let x = 0; x < item.length; i++) {
+      if (item[i].innerHTML === "X" && item.innerHTML === "X") {
+        console.log("yes");
+      }
+    }
+  });
+}
+
+gameBoard.addEventListener("click", (e) => {
+  e.preventDefault();
+  // startGame()
+  playerXMoves();
+});
